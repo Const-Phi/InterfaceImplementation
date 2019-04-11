e542ec2f2e3e43c39d77967d735ce60a055612be
Constantine Philipchenko <konstantin-649@mail.ru>
2019-04-12 00:47:23 +0300

    Пример по лекциям от 3 и 10 апреля 2019 года.

---
+++ InterfaceImplementationDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
+using static System.Environment;
+
+using InterfaceImplementationLibrary;
+
+namespace InterfaceImplementationDemo
+{
+	internal static class Program
+	{
+		public static void Main()
+		{
+			var originalListOfPersons = new List<Person>
+			{
+				Person.Parse(
+					"Бродский Иосиф Александрович",
+					"1940.05.24",
+					"1996.01.28"),
+				Person.Parse(
+					"Полозкова Вера Николаевна",
+					"1986.03.05"),
+				Person.Parse(
+					"Цветаева Марина Ивановна",
+					"1892-10-08",
+					"1941-08-31"),
+				Person.Parse(
+					"Ахматова Анна Андреевна",
+					"1889-06-23",
+					"1966-03-05"),
+				Person.Parse(
+					"Маяковский Владимир Владимирович",
+					"1893-07-19",
+					"1930-04-14"),
+				Person.Parse(
+					"Есенин Сергей Александрович",
+					"1895-10-03",
+					"1925-12-28")
+			};
+
+			Print(originalListOfPersons, "Перед сортировками:");
+
+			var sortedByAge = new List<Person>(originalListOfPersons);
+			sortedByAge.Sort(new Person.AgeComparer());
+			Print(sortedByAge, $"{NewLine}Упорядочены по возрасту:");
+
+			var sortedByBirthDay = new List<Person>(originalListOfPersons);
+			sortedByBirthDay.Sort(new Person.BirthDayComparer());
+			Print(sortedByBirthDay, $"{NewLine}Упорядочены по дате рождения:");
+
+			var sortedByFullName = new List<Person>(originalListOfPersons);
+			sortedByFullName.Sort(new Person.FullNameComparer());
+			Print(sortedByFullName, $"{NewLine}Упорядочены по имени:");
+
+			var list = originalListOfPersons.Sort(x => x.Age, (x, y) => x - y);
+			
+			ReadKey(true);
+		}
+
+		private static void Print(IEnumerable<Person> persons, string message = null)
+		{
+			WriteLine(message);
+
+			foreach (var person in persons) WriteLine(person);
+		}
+	}
+
+	public static class MethodExtensions
+	{
+		public static IEnumerable<TItem> Sort<TItem, TProperty>(
+			this IEnumerable<TItem> collection,
+			Func<TItem, TProperty> propertySelector,
+			Func<TProperty, TProperty, int> propertyComparer)
+		{
+			var copy = new List<TItem>(collection);
+			for (var i = 0; i < copy.Count; ++i)
+				for (var j = i + 1; j < copy.Count; ++j)
+					if (propertyComparer(propertySelector(copy[i]), propertySelector(copy[j])) > 0)
+					{
+						var tmp = copy[i];
+						copy[i] = copy[j];
+						copy[j] = tmp;
+					}
+
+			return copy;
+		}
+	}
+}
---
+++ InterfaceImplementationLibrary/Person.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceImplementationLibrary
+{
+	public sealed class Person
+	{
+		/// <summary>
+		/// Персона с ФИО, датой рождения и датой смерти (опционально).
+		/// </summary>
+		/// <param name="fullName">Полное имя персоны (фамилия, имя и отчество, записанные через пробел).</param>
+		/// <param name="birthDay">Дата и время (опционально) рождения по новому стилю.</param>
+		/// <param name="deathDay">Дата и время (опционально) смерти по новому стилю.</param>
+		/// <exception cref="ArgumentNullException"/>
+		/// <exception cref="ArgumentOutOfRangeException"/>
+		public Person(string fullName, DateTime birthDay, DateTime? deathDay = null)
+		{
+			FullName = fullName;
+			BirthDay = birthDay;
+			DeathDay = deathDay;
+		}
+
+		/// <summary>
+		/// Статический метод разбора. По строковому значению получаем персона.
+		/// </summary>
+		/// <param name="fullName">Полное имя персоны (фамилия, имя и отчество, записанные через пробел).</param>
+		/// <param name="birthDay">Строковое представление даты и времени (опционально) рождения по новому стилю.</param>
+		/// <param name="deathDay">Строковое представление даты и времени (опционально) смерти по новому стилю.</param>
+		/// <returns>Персона, построенная по набору строк.</returns>
+		/// <exception cref="ArgumentNullException"/>
+		/// <exception cref="ArgumentOutOfRangeException"/>
+		public static Person Parse(string fullName, string birthDay, string deathDay = null)
+		{
+			DateTime? deathDate = null;
+			if (deathDay != null)
+				deathDate = DateTime.Parse(deathDay);
+
+			return new Person(fullName, DateTime.Parse(birthDay), deathDate);
+		}
+
+		public string FullName
+		{
+			get => _fullName;
+			private set
+			{
+				if (value is null)
+					throw new ArgumentNullException(nameof(FullName));
+
+				var trimmedValue = value.Trim();
+				if (trimmedValue.Length == 0)
+					throw new ArgumentOutOfRangeException(nameof(FullName));
+
+				_fullName = trimmedValue;
+			}
+		}
+
+		public DateTime BirthDay
+		{
+			get => _birthDay;
+			private set
+			{
+				if (value > DateTime.Now)
+					throw new ArgumentOutOfRangeException(nameof(BirthDay));
+
+				_birthDay = value;
+			}
+		}
+
+		public DateTime? DeathDay
+		{
+			get => _deathDay;
+			private set
+			{
+				if (value.HasValue && value <= _birthDay)
+					throw new ArgumentOutOfRangeException(nameof(BirthDay));
+
+				_deathDay = value;
+			}
+		}
+
+		public int Age => (DeathDay ?? DateTime.Now).Year - BirthDay.Year;
+
+		public override string ToString()
+		{
+			return DeathDay.HasValue
+				? $"{FullName} ({BirthDay.ToLongDateString()} — {DeathDay.Value.ToLongDateString()})"
+				: $"{FullName} ({BirthDay.ToLongDateString()})";
+		}
+
+		private string _fullName;
+
+		private DateTime _birthDay;
+
+		private DateTime? _deathDay;
+
+		public sealed class AgeComparer : IComparer<Person>
+		{
+			public int Compare(Person x, Person y) => x?.Age ?? 0 - y?.Age ?? 0;
+		}
+
+		public sealed class BirthDayComparer : IComparer<Person>
+		{
+			/// <exception cref="ArgumentNullException"></exception>
+			public int Compare(Person firstPerson, Person secondPerson)
+			{
+				if (firstPerson is null)
+					throw new ArgumentNullException(nameof(firstPerson));
+
+				if (secondPerson is null)
+					throw new ArgumentNullException(nameof(secondPerson));
+
+				return DateTime.Compare(firstPerson.BirthDay, secondPerson.BirthDay);
+			}
+		}
+
+		public sealed class FullNameComparer : IComparer<Person>
+		{
+			public int Compare(Person x, Person y) => string.CompareOrdinal(x?.FullName, y?.FullName);
+		}
+	}
+}
